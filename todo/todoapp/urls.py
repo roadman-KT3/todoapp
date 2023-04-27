2808842de140754b8be464a67f2cf1e3fdad978a
KT-hue-pixel <nanalwelu1997@gmail.com>
2023-04-27 21:12:56 +0000

    first commmit

---
+++ todo/todoapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import TodoList, TodoCreate, TodoDetail, TodoUpdate, TodoDelete, home
+urlpatterns = [
+    path('home/', home, name='home'),
+    path('list', TodoList.as_view(), name='todo-list'),
+    path('create', TodoCreate.as_view(), name='todo-create'),
+    path('<int:pk>', TodoDetail.as_view(), name='todo-detail'),
+    path('<int:pk>/update', TodoUpdate.as_view(), name='todo-update'),
+    path('<int:pk>/delete', TodoDelete.as_view(), name='todo-delete'),
+]
---
+++ todo/todoapp/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Todo
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+class TodoCreate(CreateView):
+    model = Todo.objects.all()
+    fields = '__all__'
+
+class TodoList(ListView):
+    model = Todo.objects.all()
+    #template_name = 'todo.html'
+    context_object_name = 'todos'
+
+class TodoDetail(DetailView):
+    model = Todo.objects.all()
+    #template_name = 'todo.html'
+    context_object_name = 'todo'
+
+class TodoUpdate(UpdateView):
+    model = Todo.objects.all()
+    fields = '__all__'
+    #template_name = 'todo.html'
+    context_object_name = 'todo'
+
+class TodoDelete(DeleteView):
+    model = Todo.objects.all()
+    #template_name = 'todo.html'
+    context_object_name = 'todo'
+
+
